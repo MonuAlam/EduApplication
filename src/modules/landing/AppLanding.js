9d716bdb27ff8754e7f603e7d9426ad7408f5887
Monu Alam <monualam828@gmail.com>
2024-10-27 12:42:49 +0530

    Landing

--- src/modules/landing/AppLanding.js
+++ src/modules/landing/AppLanding.js
@@ -51,7 +51,7 @@ function AppLanding() {
                                 </div>
                             </div>
                             <div className='row mt-5'>
-                            <div className='col-md-3 d-flex justify-content-center '>
+                            <div className='col-md-3 d-flex justify-content-center mt-5'>
                             <Link to={"paymentLanding"}>
                                         <div className="card shadow bg-warning h-100" style={{ width: "12rem", alignItems: "center" }}>
                                             <img src={Payment} className="card-img-top mt-2 w-50 h-40" alt="..." />
@@ -61,7 +61,7 @@ function AppLanding() {
                                         </div>
                                     </Link>
                                 </div>
-                                <div className='col-md-3 d-flex justify-content-center '>
+                                <div className='col-md-3 d-flex justify-content-center mt-5'>
                                     <Link to={"paymentLanding"}>
                                         <div className="card shadow bg-warning h-100" style={{ width: "12rem", alignItems: "center" }}>
                                             <img src={Payment} className="card-img-top mt-2 w-50 h-40" alt="..." />
@@ -71,7 +71,7 @@ function AppLanding() {
                                         </div>
                                     </Link>
                                 </div>
-                                <div className='col-md-3 d-flex justify-content-center '>
+                                <div className='col-md-3 d-flex justify-content-center mt-5'>
                                     <Link to={"paymentLanding"}>
                                         <div className="card shadow bg-warning h-100" style={{ width: "12rem", alignItems: "center" }}>
                                             <img src={Payment} className="card-img-top mt-2 w-50 h-40" alt="..." />
@@ -81,7 +81,7 @@ function AppLanding() {
                                         </div>
                                     </Link>
                                 </div>
-                                <div className='col-md-3 d-flex justify-content-center '>
+                                <div className='col-md-3 d-flex justify-content-center mt-5'>
                                     <Link to={"paymentLanding"}>
                                         <div className="card shadow bg-warning h-100" style={{ width: "12rem", alignItems: "center" }}>
                                             <img src={Payment} className="card-img-top mt-2 w-50 h-40" alt="..." />
